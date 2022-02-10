490cac536010dca9eef21bf8342571cbb89c961c
Marco Cafaro <marco2.cafaro@mail.polimi.it>
2022-02-10 16:12:39 +0100

    Added temporary logo

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,21 +1,27 @@
 <template>
   <v-container fluid>
-    <v-row class="text-center">
+    <v-row class="text-center" no-gutters>
+      <v-col cols="12">
+        <router-link to="/about">About</router-link>
+      </v-col>
       <v-col cols="12">
         <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
+          :src="require('../assets/logo2.png')"
+          class="my-0"
           contain
-          height="40vh"
+          height="50vh"
+          large
         />
       </v-col>
       <v-col cols="12">
         <v-btn
           elevation="2"
           fab
-          height="20vh"
-          width="20vh"
-        >play</v-btn>
+          height="30vh"
+          width="30vh"
+          to="/main">
+            <span style="font-size: 5vh;">play</span>
+          </v-btn>
       </v-col>
     </v-row>
   </v-container>
